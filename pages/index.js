1cbd81d016cd4e5abd60a62e6a887b2e6e087a13
Ben Clark <hello@clarkben.com>
2023-01-08 21:51:38 +0000

    Adds a bit of a dummy page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
-  }
-  
-  export default HomePage
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>
+                        <button>View 1</button>
+                    </li>
+                    <li>
+                        <button>View 2</button>
+                    </li>
+                    <li>
+                        <button>View 3</button>
+                    </li>
+                </ul>
+            </nav>
+            <main>
+                <img src="https://images.unsplash.com/photo-1671877308526-ac799c88db13"></img>
+            </main>
+        </div>
+    );
+}
+
+export default HomePage;
